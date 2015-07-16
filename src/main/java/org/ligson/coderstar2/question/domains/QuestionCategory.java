1f859abe12c311fef1cf79e6aadb5c1e0c0afb5f
ligson <593949938@qq.com>
2015-07-16 03:26:50 +0000

    标签类增加

--- src/main/java/org/ligson/coderstar2/question/domains/QuestionCategory.java
+++ src/main/java/org/ligson/coderstar2/question/domains/QuestionCategory.java
@@ -2,11 +2,45 @@ package org.ligson.coderstar2.question.domains;
 
 import org.ligson.coderstar2.system.domains.Category;
 
+import javax.persistence.*;
+
 /**
  * Created by ligson on 2015/7/16.
+ * 问题分类对应
  */
+@Entity
+@Table(name = "question_categroy")
 public class QuestionCategory {
     private long id;
     private Question question;
     private Category category;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    @Column(nullable = false, name = "question_id")
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Column(nullable = false, name = "category_id")
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
---
+++ src/main/java/org/ligson/coderstar2/system/domains/SysTag.java
@@ -0,0 +1,75 @@
+package org.ligson.coderstar2.system.domains;
+
+import com.boful.common.date.utils.DateUtils;
+import org.ligson.coderstar2.user.domains.User;
+
+import javax.persistence.*;
+
+/**
+ * Created by ligson on 2015/7/16.
+ */
+@Entity
+@Table(name = "sys_tag")
+public class SysTag {
+    private long id;
+    private String name;
+    private String createDate = DateUtils.format();
+    private User creator;
+    private long questionNum = 0;
+    private long articleNum = 0;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "create_date", nullable = false)
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    @Column(name = "creator_id", nullable = false)
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    @Column(name = "question_num", nullable = false)
+    public long getQuestionNum() {
+        return questionNum;
+    }
+
+    public void setQuestionNum(long questionNum) {
+        this.questionNum = questionNum;
+    }
+
+    @Column(name = "article_num", nullable = false)
+    public long getArticleNum() {
+        return articleNum;
+    }
+
+    public void setArticleNum(long articleNum) {
+        this.articleNum = articleNum;
+    }
+}
