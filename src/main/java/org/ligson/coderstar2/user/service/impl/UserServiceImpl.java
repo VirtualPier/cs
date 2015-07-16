0b133966261fcd3d767fb8fda62e6884ff61c72b
ligson <593949938@qq.com>
2015-07-16 08:47:11 +0000

    spring服务配置文件

--- src/main/java/org/ligson/coderstar2/user/service/impl/UserServiceImpl.java
+++ src/main/java/org/ligson/coderstar2/user/service/impl/UserServiceImpl.java
@@ -1,9 +1,19 @@
 package org.ligson.coderstar2.user.service.impl;
 
+import org.ligson.coderstar2.user.dao.UserDao;
 import org.ligson.coderstar2.user.service.UserService;
 
 /**
  * Created by ligson on 2015/7/16.
  */
 public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
 }
