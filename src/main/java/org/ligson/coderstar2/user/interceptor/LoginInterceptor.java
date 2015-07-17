f0351182d9c752db680e6bba4d7699236cfc6dba
ligson <593949938@qq.com>
2015-07-17 07:06:45 +0000

    后台用户列表页面

--- src/main/java/org/ligson/coderstar2/user/interceptor/LoginInterceptor.java
+++ src/main/java/org/ligson/coderstar2/user/interceptor/LoginInterceptor.java
@@ -1,10 +1,14 @@
 package org.ligson.coderstar2.user.interceptor;
 
+import org.ligson.coderstar2.controllers.AdminController;
+import org.ligson.coderstar2.user.admin.controllers.UserMgrController;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 /**
  * Created by ligson on 2015/7/16.
@@ -13,7 +17,42 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Object bean = handlerMethod.getBean();
+            if (bean instanceof AdminController) {
+                Method method = handlerMethod.getMethod();
+                String methodName = method.getName();
+                String[] uncheckAction = new String[]{"login", "checkLogin"};
+                boolean isExsit = false;
+                for (String actionName : uncheckAction) {
+                    if (actionName.equals(methodName)) {
+                        isExsit = true;
+                        break;
+                    }
+                }
+                if (!isExsit) {
+                    return adminLoginCheck(request, response);
+                }
+            }
+
+            if (bean instanceof UserMgrController) {
+                return adminLoginCheck(request, response);
+            }
+        }
         return true;
+    }
+
+    private boolean adminLoginCheck(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object object = request.getSession().getAttribute("adminUser");
+        if (object == null) {
+            request.setAttribute("success", false);
+            request.setAttribute("msg", "请登录!");
+            request.getRequestDispatcher("/admin/login").forward(request, response);
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
