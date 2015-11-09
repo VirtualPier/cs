2f6b601ec210d4310e91705c5a11d67d0eee6f16
ligson <593949938@qq.com>
2015-11-09 15:18:26 +0800

    增加机构设计

---
+++ src/main/java/org/ligson/coderstar2/orgz/service/impl/OrgzServiceImpl.java
@@ -0,0 +1,30 @@
+package org.ligson.coderstar2.orgz.service.impl;
+
+import org.ligson.coderstar2.orgz.domains.OrgDetail;
+import org.ligson.coderstar2.orgz.service.OrgzService;
+
+import java.util.List;
+
+/**
+ * Created by ligson on 2015/11/9.
+ */
+public class OrgzServiceImpl implements OrgzService {
+    @Override
+    public void regOrg(OrgDetail orgDetail) {
+    }
+
+    @Override
+    public void auditOrg(OrgDetail orgDetail) {
+
+    }
+
+    @Override
+    public List<OrgDetail> query(OrgDetail orgDetail, int max, int offset) {
+        return null;
+    }
+
+    @Override
+    public List<OrgDetail> count(OrgDetail orgDetail) {
+        return null;
+    }
+}
