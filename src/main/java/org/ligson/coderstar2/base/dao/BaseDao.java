a33eab270b1c1a5d99c03c78399d1f906b2d7ca3
ligson <593949938@qq.com>
2015-11-30 10:54:28 +0800

    js压缩

--- src/main/java/org/ligson/coderstar2/base/dao/BaseDao.java
+++ src/main/java/org/ligson/coderstar2/base/dao/BaseDao.java
@@ -4,23 +4,86 @@ import org.ligson.coderstar2.user.domains.User;
 
 import java.util.List;
 
+/***
+ * hibernate dao封装
+ *
+ * @param <T>
+ */
 public interface BaseDao<T> {
+
+    /***
+     * save操作
+     *
+     * @param t
+     * @return
+     */
     public String add(T t);
 
+    /***
+     * save or update 操作
+     *
+     * @param t
+     */
     public void saveOrUpdate(T t);
 
+    /***
+     * 删除操作
+     *
+     * @param t
+     */
     public void delete(T t);
 
+    /***
+     * 更新操作
+     *
+     * @param t
+     */
     public void update(T t);
 
+    /***
+     * 更新某一个属性
+     *
+     * @param property
+     * @param propertyValue
+     * @param id
+     */
     public void updateProperty(String property, String propertyValue, long id);
 
+    /***
+     * 通过id查找对象
+     *
+     * @param id
+     * @return
+     */
     public T getById(long id);
 
+    /***
+     * 通过一个属性查询
+     *
+     * @param propertyName
+     * @param propertyValue
+     * @return
+     */
     public T findBy(String propertyName, Object propertyValue);
 
+    /***
+     * 通过多个属性查询一个
+     *
+     * @param propertyNames
+     * @param propertyValues
+     * @return
+     */
     public T findByAnd(List<String> propertyNames, List<Object> propertyValues);
 
+    /***
+     * 查询多个
+     *
+     * @param propertyNames
+     * @param propertyValues
+     * @param max
+     * @param offset
+     * @return
+     */
     public List<T> findAllByAnd(List<String> propertyNames, List<Object> propertyValues, int max, int offset);
 
     public List<T> findAllBy(String propertyName, Object propertyValue);
